61875d4843b65d5d136a84661a1fce7a7bd70f6b
Herdin Hidayat <hidayatyaa@gmail.com>
2024-12-23 22:07:04 +0800

    nambah inter css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Halaman Home</title>
     @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <title>Halaman Home</title>
 </head>
 
 <body>
