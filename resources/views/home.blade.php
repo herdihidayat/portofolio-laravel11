c750c3f8128735f8bbdf6a3412ec55956d5af777
Herdin Hidayat <hidayatyaa@gmail.com>
2024-12-26 14:06:04 +0800

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,12 +15,7 @@
 
     <div class="min-h-full">
         <x-navbar></x-navbar>
-
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
-            </div>
-        </header>
+        <x-header>Home Page</x-header>
 
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
