c4dafa70b346a64bcd63f0195ec261a590ae809c
Herdin Hidayat <hidayatyaa@gmail.com>
2025-01-01 20:55:22 +0800

    error title done

--- resources/views/blog.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">ini adalah halaman blog</h3>
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,21 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Herdin Hidayat', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Herdin Hidayat',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime voluptates et dolorum. Culpa, ad? At quaerat beatae saepe a. Omnis optio neque voluptatem quae modi, nobis ea placeat cum architecto.'
+        ],
+        [
+
+            'title' => 'Judul Artikel 2',
+            'author' => 'Herdin Hidayat',
+            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime voluptates et dolorum. Culpa, ad? At quaerat beatae saepe a. Omnis optio neque voluptatem quae modi, nobis ea placeat cum architecto.'
+
+        ]
+    ]]);
 });
 
 Route::get('/contact', function () {
