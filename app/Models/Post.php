30ddca5f59019635aecd7452dfe560a2abd1af4c
Herdin Hidayat <hidayatyaa@gmail.com>
2025-01-20 15:37:24 +0800

    kembali db, pergantain slug ganti id

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,41 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Post
-{
-    public static function all()
-    {
-        return [
-            [
-                'id' => '1',
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Herdin Hidayat',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime voluptates et dolorum. Culpa, ad? At quaerat beatae saepe a. Omnis optio neque voluptatem quae modi, nobis ea placeat cum architecto.'
-            ],
-            [
-                'id' => '2',
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Herdin Hidayat',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime voluptates et dolorum. Culpa, ad? At quaerat beatae saepe a. Omnis optio neque voluptatem quae modi, nobis ea placeat cum architecto.'
-
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-        // return Arr::first(static::all(), function ($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-
-        if (! $post) {
-            abort(404);
-        }
-        return $post;
-    }
-}
+class Post extends Model // tabel blog_posts
+{}
--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
+Route::get('/posts/{id}', function ($id) {
 
-    $post = Post::find($slug);
+    $post = Post::find($id);
 
     return view('post', ['title' => 'Singel Post', 'post' => $post]);
 });
