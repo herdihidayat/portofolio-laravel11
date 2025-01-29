6d27b100fdd8f3f4dfd3461ce006dc83e9cc50cb
Herdin Hidayat <hidayatyaa@gmail.com>
2025-01-29 20:20:12 +0800

    bebrapa edit sudah ntar dirumah lagi

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
 
 
     <div class="py-4 px-4 mx-auto max-w-screen-xl lg:py-8 lg:px-0">
-        <div class="grid gap-8 lg:grid-cols-3">
+        <div class="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
 
             @foreach ($posts as $post)
                 {{-- <article class="py-8 max-w-screen-md border-b border-gray-300">
@@ -28,19 +28,14 @@
                     class="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div class="flex justify-between items-center mb-5 text-gray-500">
                         <span
-                            class="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
-                            <svg class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z">
-                                </path>
-                            </svg>
-                            Tutorial
+                            class="bg-green-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+
+                            {{ $post->category->name }}
                         </span>
                         <span class="text-sm">14 days ago</span>
                     </div>
-                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a
-                            href="#">How to quickly deploy a static website</a></h2>
+                    <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"><a href="#">How
+                            to quickly deploy a static website</a></h2>
                     <p class="mb-5 font-light text-gray-500 dark:text-gray-400">Static websites are now used to
                         bootstrap lots of websites and are becoming the basis for a variety of tools that even
                         influence both web designers and developers influence both web designers and developers.</p>
